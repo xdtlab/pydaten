363a92c8dd896e3c4189889d537e08d7fe8e0522
Daten Project <daten-project@protonmail.ch>
2018-09-14 15:35:32 +0430

    refactoring

--- pydaten/core/world.py
+++ pydaten/core/world.py
@@ -6,8 +6,17 @@ from pydaten.common.address import Address, RawAddress, NameAddress
 from pydaten.utils.bytestream import ByteStream
 from pydaten.defaults import genesis, config
 import struct
+
+def _tx_key(name_address):
+    return b'.'.join([n.encode('ascii') for n in reversed(name_address.name)])
+
 class World:
     HEIGHT_PREFIX = b'\x00'
+    HEADER_PREFIX = b'\x01'
+    BLOCK_TRANSACTION_PREFIX = b'\x02'
+    SHORTCUT_PREFIX = b'\x03'
+    RESOLVE_PREFIX = b'\x04'
+    BALANCE_PREFIX = b'\x05'
 
     def __init__(self, path):
         self.root = plyvel.DB(path, create_if_missing = True)
@@ -25,31 +34,30 @@ class World:
         addr = transaction.address()
         name, rest = addr.pop()
         bs = ByteStream()
-        bs.write(b'.'.join([n.encode('ascii') for n in reversed(rest.name)]))
+        bs.write(_tx_key(rest))
         bs.write(b'|')
         bs.write_uint32(transaction.target)
         bs.write_uint16(index)
         bs.write(name.encode('ascii'))
         k = bs.value()
         wb.put(k, transaction.serialize())
-        shortcut = b'.'.join([n.encode('ascii') for n in reversed(addr.name)])
-        wb.put(b'\x03' + shortcut, k)
+        shortcut = _tx_key(addr)
+        wb.put(World.SHORTCUT_PREFIX + shortcut, k)
         bs = ByteStream()
         src = self.resolve(transaction.source)
         src.write(bs)
-        wb.put(b'\x04' + shortcut, bs.value())
+        wb.put(World.RESOLVE_PREFIX + shortcut, bs.value())
         dst = self.resolve(transaction.destination)
         return k
 
     def get_transaction(self, name_address):
-        shortcut = b'.'.join([n.encode('ascii') for n in reversed(name_address.name)])
-        k = self.root.get(b'\x03' + shortcut)
+        k = self.root.get(World.SHORTCUT_PREFIX + _tx_key(name_address))
         return Transaction.deserialize(self.root.get(k))
     def clear_transaction(self, wb, name_address):
-        shortcut = b'.'.join([n.encode('ascii') for n in reversed(name_address.name)])
-        k = self.root.get(b'\x03' + shortcut)
-        wb.delete(b'\x03' + shortcut)
-        wb.delete(b'\x04' + shortcut)
+        shortcut = _tx_key(name_address)
+        k = self.root.get(World.SHORTCUT_PREFIX + shortcut)
+        wb.delete(World.SHORTCUT_PREFIX + shortcut)
+        wb.delete(World.RESOLVE_PREFIX + shortcut)
         wb.delete(k)
 
     def push_block(self, block):
@@ -68,11 +76,11 @@ class World:
         return latest
 
     def set_block(self, wb, block):
-        wb.put(b'\x01' + struct.pack('>L', block.index), block.serialize(header_only = True))
+        wb.put(World.HEADER_PREFIX + struct.pack('>L', block.index), block.serialize(header_only = True))
         balance = {}
         for ind, tx in enumerate(block.transactions):
             k = self.set_transaction(wb, ind, tx)
-            wb.put(b'\x02' + struct.pack('>LL', block.index, ind), k)
+            wb.put(World.BLOCK_TRANSACTION_PREFIX + struct.pack('>LL', block.index, ind), k)
             src = self.resolve(tx.source)
             dst = self.resolve(tx.destination)
             if src.public_key not in balance:
@@ -85,14 +93,14 @@ class World:
             if raw != config.NOWHERE_NAME:
                 self.set_balance(wb, raw, self.get_balance(raw) + bal)
     def get_block(self, index):
-        block = Block.deserialize(self.root.get(b'\x01' + struct.pack('>L', index)), header_only = True)
-        prefix = b'\x02' + struct.pack('>L', index)
+        block = Block.deserialize(self.root.get(World.HEADER_PREFIX + struct.pack('>L', index)), header_only = True)
+        prefix = World.BLOCK_TRANSACTION_PREFIX + struct.pack('>L', index)
         txs = list(self.root.iterator(prefix = prefix, include_key = False))
         block.transactions = [Transaction.deserialize(self.root.get(tx)) for tx in txs]
         return block
     def clear_block(self, wb, index):
-        wb.delete(b'\x01' + struct.pack('>L', index))
-        prefix = b'\x02' + struct.pack('>L', index)
+        wb.delete(World.HEADER_PREFIX + struct.pack('>L', index))
+        prefix = World.BLOCK_TRANSACTION_PREFIX + struct.pack('>L', index)
         balance = {}
         for tx in self.root.iterator(prefix = prefix, include_value = False):
             transact = Transaction.deserialize(self.root.get(self.root.get(tx)))
@@ -115,26 +123,25 @@ class World:
         return self.get_block(self.get_height() - 1)
 
     def get_balance(self, raw_address):
-        k = b'\x05' + raw_address.public_key
+        k = World.BALANCE_PREFIX + raw_address.public_key
         result = self.root.get(k)
         return struct.unpack('>Q', result)[0] if result else 0
     def set_balance(self, wb, raw_address, balance):
-        k = b'\x05' + raw_address.public_key
+        k = World.BALANCE_PREFIX + raw_address.public_key
         wb.put(k, struct.pack('>Q', balance))
 
     def resolve(self, address):
         if type(address) is RawAddress:
             return address
         else:
-            shortcut = b'.'.join([n.encode('ascii') for n in reversed(address.name)])
-            result = self.root.get(b'\x04' + shortcut)
+            result = self.root.get(World.RESOLVE_PREFIX + _tx_key(address))
             if result:
                 return Address.read(ByteStream(result))
             else:
                 raise Exception("Invalid name!")
 
     def children(self, name_address):
-        prefix = b'.'.join([n.encode('ascii') for n in reversed(name_address.name)]) + b'|'
+        prefix = _tx_key(name_address) + b'|'
         result = []
         for k, v in self.root.iterator(prefix = prefix):
             result.append(Transaction.deserialize(v))
