b25ff30a6bcc257dfaab87801ac90a953820469c
Daten Project <daten-project@protonmail.ch>
2018-09-15 02:15:47 +0430

    more beautiful

--- pydaten/core/world.py
+++ pydaten/core/world.py
@@ -22,113 +22,20 @@ class World:
     def __init__(self, path):
         self.root = plyvel.DB(path, create_if_missing = True, paranoid_checks = True)
 
-    def get_height(self):
-        result = self.root.get(World.HEIGHT_PREFIX)
-        return struct.unpack('>L', result)[0] if result else 0
-    def set_height(self, wb, height):
-        if height > 0:
-            wb.put(World.HEIGHT_PREFIX, struct.pack('>L', height))
-        else:
-            wb.delete(World.HEIGHT_PREFIX)
-
-    def set_transaction(self, wb, index, transaction):
-        addr = transaction.address()
-        name, rest = addr.pop()
-        bs = ByteStream()
-        bs.write(World.TRANSACTION_PREFIX + _tx_key(rest) + b'|')
-        bs.write_uint32(transaction.target)
-        bs.write_uint16(index)
-        bs.write(name.encode('ascii'))
-        k = bs.value()
-        wb.put(k, transaction.serialize())
-        addr_key = _tx_key(addr)
-        wb.put(World.SHORTCUT_PREFIX + addr_key, k)
-        bs = ByteStream()
-        src = self.resolve(transaction.source)
-        src.write(bs)
-        wb.put(World.RESOLVE_PREFIX + addr_key, bs.value())
-        dst = self.resolve(transaction.destination)
-        return k
-
-    def get_transaction(self, name_address):
-        k = self.root.get(World.SHORTCUT_PREFIX + _tx_key(name_address))
-        return Transaction.deserialize(self.root.get(k))
-    def clear_transaction(self, wb, name_address):
-        addr_key = _tx_key(name_address)
-        k = self.root.get(World.SHORTCUT_PREFIX + addr_key)
-        wb.delete(World.SHORTCUT_PREFIX + addr_key)
-        wb.delete(World.RESOLVE_PREFIX + addr_key)
-        wb.delete(k)
-
     def push_block(self, block):
         height = self.get_height()
         if block.index == height:
             with self.root.write_batch() as wb:
-                self.set_height(wb, height + 1)
-                self.set_block(wb, block)
+                self._set_height(wb, height + 1)
+                self._set_block(wb, block)
         else:
             raise Exception("Block Index mismatch!")
     def pop_block(self):
         latest = self.get_latest_block()
         with self.root.write_batch() as wb:
-            self.set_height(wb, self.get_height() - 1)
-            self.clear_block(wb, latest.index)
+            self._set_height(wb, self.get_height() - 1)
+            self._clear_block(wb, latest.index)
         return latest
-
-    def set_block(self, wb, block):
-        wb.put(World.HEADER_PREFIX + struct.pack('>L', block.index), block.serialize(header_only = True))
-        balance = {}
-        for ind, tx in enumerate(block.transactions):
-            k = self.set_transaction(wb, ind, tx)
-            wb.put(World.BLOCK_TRANSACTION_PREFIX + struct.pack('>LL', block.index, ind), k)
-            src = self.resolve(tx.source)
-            dst = self.resolve(tx.destination)
-            if src not in balance:
-                balance[src] = 0
-            if dst not in balance:
-                balance[dst] = 0
-            balance[src] -= (tx.fee + tx.amount)
-            balance[dst] += tx.amount
-        for raw, bal in balance.items():
-            if raw != config.NOWHERE_NAME:
-                self.set_balance(wb, raw, self.get_balance(raw) + bal)
-    def get_block(self, index):
-        block = Block.deserialize(self.root.get(World.HEADER_PREFIX + struct.pack('>L', index)), header_only = True)
-        prefix = World.BLOCK_TRANSACTION_PREFIX + struct.pack('>L', index)
-        txs = list(self.root.iterator(prefix = prefix, include_key = False))
-        block.transactions = [Transaction.deserialize(self.root.get(tx)) for tx in txs]
-        return block
-    def clear_block(self, wb, index):
-        wb.delete(World.HEADER_PREFIX + struct.pack('>L', index))
-        prefix = World.BLOCK_TRANSACTION_PREFIX + struct.pack('>L', index)
-        balance = {}
-        for tx in self.root.iterator(prefix = prefix, include_value = False):
-            transact = Transaction.deserialize(self.root.get(self.root.get(tx)))
-            addr = transact.address()
-            self.clear_transaction(wb, addr)
-            src = self.resolve(transact.source)
-            dst = self.resolve(transact.destination)
-            if src not in balance:
-                balance[src] = 0
-            if dst not in balance:
-                balance[dst] = 0
-            balance[src] += (transact.fee + transact.amount)
-            balance[dst] -= transact.amount
-            wb.delete(tx)
-        for raw, bal in balance.items():
-            if raw != config.NOWHERE_NAME:
-                self.set_balance(wb, raw, self.get_balance(raw) + bal)
-
-    def get_latest_block(self):
-        return self.get_block(self.get_height() - 1)
-
-    def get_balance(self, raw_address):
-        k = World.BALANCE_PREFIX + raw_address.public_key
-        result = self.root.get(k)
-        return struct.unpack('>Q', result)[0] if result else 0
-    def set_balance(self, wb, raw_address, balance):
-        k = World.BALANCE_PREFIX + raw_address.public_key
-        wb.put(k, struct.pack('>Q', balance))
 
     def resolve(self, address):
         if type(address) is RawAddress:
@@ -146,3 +53,94 @@ class World:
         for k, v in self.root.iterator(prefix = prefix):
             result.append(Transaction.deserialize(v))
         return result
+
+    def get_height(self):
+        result = self.root.get(World.HEIGHT_PREFIX)
+        return struct.unpack('>L', result)[0] if result else 0
+    def _set_height(self, wb, height):
+        if height > 0:
+            wb.put(World.HEIGHT_PREFIX, struct.pack('>L', height))
+        else:
+            wb.delete(World.HEIGHT_PREFIX)
+
+    def _set_transaction(self, wb, index, transaction):
+        addr = transaction.address()
+        name, rest = addr.pop()
+        bs = ByteStream()
+        bs.write(World.TRANSACTION_PREFIX + _tx_key(rest) + b'|')
+        bs.write_uint32(transaction.target)
+        bs.write_uint16(index)
+        bs.write(name.encode('ascii'))
+        k = bs.value()
+        wb.put(k, transaction.serialize())
+        addr_key = _tx_key(addr)
+        wb.put(World.SHORTCUT_PREFIX + addr_key, k)
+        bs = ByteStream()
+        src = self.resolve(transaction.source)
+        src.write(bs)
+        wb.put(World.RESOLVE_PREFIX + addr_key, bs.value())
+        dst = self.resolve(transaction.destination)
+        return k
+    def get_transaction(self, name_address):
+        k = self.root.get(World.SHORTCUT_PREFIX + _tx_key(name_address))
+        return Transaction.deserialize(self.root.get(k))
+    def _clear_transaction(self, wb, name_address):
+        addr_key = _tx_key(name_address)
+        k = self.root.get(World.SHORTCUT_PREFIX + addr_key)
+        wb.delete(World.SHORTCUT_PREFIX + addr_key)
+        wb.delete(World.RESOLVE_PREFIX + addr_key)
+        wb.delete(k)
+
+    def _set_block(self, wb, block):
+        wb.put(World.HEADER_PREFIX + struct.pack('>L', block.index), block.serialize(header_only = True))
+        balance = {}
+        for ind, tx in enumerate(block.transactions):
+            k = self._set_transaction(wb, ind, tx)
+            wb.put(World.BLOCK_TRANSACTION_PREFIX + struct.pack('>LL', block.index, ind), k)
+            src = self.resolve(tx.source)
+            dst = self.resolve(tx.destination)
+            if src not in balance:
+                balance[src] = 0
+            if dst not in balance:
+                balance[dst] = 0
+            balance[src] -= (tx.fee + tx.amount)
+            balance[dst] += tx.amount
+        for raw, bal in balance.items():
+            if raw != config.NOWHERE_NAME:
+                self._set_balance(wb, raw, self.get_balance(raw) + bal)
+    def get_block(self, index):
+        block = Block.deserialize(self.root.get(World.HEADER_PREFIX + struct.pack('>L', index)), header_only = True)
+        prefix = World.BLOCK_TRANSACTION_PREFIX + struct.pack('>L', index)
+        txs = list(self.root.iterator(prefix = prefix, include_key = False))
+        block.transactions = [Transaction.deserialize(self.root.get(tx)) for tx in txs]
+        return block
+    def _clear_block(self, wb, index):
+        wb.delete(World.HEADER_PREFIX + struct.pack('>L', index))
+        prefix = World.BLOCK_TRANSACTION_PREFIX + struct.pack('>L', index)
+        balance = {}
+        for tx in self.root.iterator(prefix = prefix, include_value = False):
+            transact = Transaction.deserialize(self.root.get(self.root.get(tx)))
+            addr = transact.address()
+            self._clear_transaction(wb, addr)
+            src = self.resolve(transact.source)
+            dst = self.resolve(transact.destination)
+            if src not in balance:
+                balance[src] = 0
+            if dst not in balance:
+                balance[dst] = 0
+            balance[src] += (transact.fee + transact.amount)
+            balance[dst] -= transact.amount
+            wb.delete(tx)
+        for raw, bal in balance.items():
+            if raw != config.NOWHERE_NAME:
+                self._set_balance(wb, raw, self.get_balance(raw) + bal)
+    def get_latest_block(self):
+        return self.get_block(self.get_height() - 1)
+
+    def get_balance(self, raw_address):
+        k = World.BALANCE_PREFIX + raw_address.public_key
+        result = self.root.get(k)
+        return struct.unpack('>Q', result)[0] if result else 0
+    def _set_balance(self, wb, raw_address, balance):
+        k = World.BALANCE_PREFIX + raw_address.public_key
+        wb.put(k, struct.pack('>Q', balance))
