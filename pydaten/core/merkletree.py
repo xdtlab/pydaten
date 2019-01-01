42ec173ee046788d0fb1806d38e9c001f833bbe0
Daten Project <daten-project@protonmail.ch>
2019-01-02 02:43:19 +0330

    Revert "remove direction map of merkletrees"
    This reverts commit 964b0193baa0579bc897eb8b2f905d3d8ab414e1.

--- pydaten/core/merkletree.py
+++ pydaten/core/merkletree.py
@@ -4,27 +4,27 @@ from pydaten.utils.bytestream import ByteStream
 from pydaten.core.errors import HashNotFound
 
 class MerkleNode:
-    def __init__(self, value, neighbour, parent):
+    def __init__(self, value, left, right, parent):
         self.value = value
-        self.neighbour = neighbour
+        self.left = left
+        self.right = right
         self.parent = parent
 
 class MerkleTree:
 
     def __init__(self, hashes):
-        self._leaves = [MerkleNode(h, None, None) for h in hashes]
+        self._leaves = [MerkleNode(h, None, None, None) for h in hashes]
         nodes = self._leaves
         while len(nodes) > 1:
             new_layer = []
             for i in range(len(nodes) // 2):
                 a = nodes[i * 2]
                 b = nodes[i * 2 + 1]
-                a, b = (a, b) if a.value < b.value else (b, a)
                 new_hash = config.REGULAR_HASH_FUNCTION(a.value + b.value)
-                parent = MerkleNode(new_hash, None, None)
+                parent = MerkleNode(new_hash, None, None, None)
                 a.parent = b.parent = parent
-                a.neighbour = b
-                b.neighbour = a
+                a.right = b
+                b.left = a
                 new_layer.append(parent)
             if len(nodes) % 2 == 1:
                 last = nodes[-1]
@@ -46,16 +46,22 @@ class MerkleTree:
         output = ByteStream()
         start = node
         while start.parent:
-            output.write(start.neighbour.value)
+            output.write_uint8(0 if start.right else 1)
+            output.write(start.right.value if start.right else start.left.value)
             start = start.parent
         return output.value()
 
     def verify_path(self, start, path):
         stream = ByteStream(path)
-        while True:
-            h = stream.read(32)
-            if len(h) != 32:
-                break
-            start, h = (start, h) if start < h else (h, start)
-            start = config.REGULAR_HASH_FUNCTION(start + h)
-        return start == self._root.value
+        try:
+            while True:
+                d = stream.read_uint8()
+                h = stream.read(32)
+                if d == 0:
+                    start = config.REGULAR_HASH_FUNCTION(start + h)
+                elif d == 1:
+                    start = config.REGULAR_HASH_FUNCTION(h + start)
+                else:
+                    return False
+        except:
+            return start == self._root.value
